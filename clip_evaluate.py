3fb45adf64d4b78c9879b4a6c6707fb465b1323f
yujin Kim <lakeeye1220@gmail.com>
2025-11-07 22:56:11 +0900

    Create clip_evaluate.py

---
+++ clip_evaluate.py
@@ -0,0 +1,36 @@
+import torch
+from torchvision import transforms
+from typing import Union, Tuple, Optional
+from transformers import CLIPModel, CLIPProcessor
+
+clip_id = "openai/clip-vit-large-patch14"
+clip_model = CLIPModel.from_pretrained(clip_id).to('cuda').eval()
+clip_proc  = CLIPProcessor.from_pretrained(clip_id)
+
+def get_text_features(text: str, norm: bool = True) -> torch.Tensor:
+
+    text_inputs = clip_proc(text=text, return_tensors="pt", padding=True).to('cuda')
+    text_features = clip_model.get_text_features(**text_inputs)
+    if norm:
+        text_features /= text_features.norm(dim=-1, keepdim=True)
+
+    return text_features
+
+def get_image_features(img: torch.Tensor, norm: bool = True) -> torch.Tensor:
+    #img_resized = torch.nn.functional.interpolate(img, size=(224, 224), mode='bilinear', align_corners=False)
+    clip_inputs = clip_proc(images=img, return_tensors="pt").to('cuda')
+    image_features = clip_model.get_image_features(**clip_inputs)
+    if norm:
+        image_features /= image_features.clone().norm(dim=-1, keepdim=True)
+
+    return image_features
+
+def img_to_img_similarity(src_images, generated_images):
+    src_img_features = get_image_features(src_images)
+    gen_img_features = get_image_features(generated_images)
+    return (src_img_features @ gen_img_features.T).mean()
+
+def txt_to_img_similarity(text, generated_images):
+    text_features    = get_text_features(text)
+    gen_img_features = get_image_features(generated_images)
+    return (text_features @ gen_img_features.T).mean()
